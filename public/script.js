5a96343b4fa1c958c4dad992ba8e7a5e2359c3ef
md2eoseo <md2eoseo@gmail.com>
2020-10-14 11:33:26 +0900

    dropdown menu on sitemap (without animation)

--- public/script.js
+++ public/script.js
@@ -2,11 +2,20 @@
 
 const start = () => {
   console.log("Hello Tailwindcss!");
-  document.querySelectorAll("a").forEach((el) =>
-    el.addEventListener("click", (e) => {
-      e.preventDefault();
-    })
-  );
+  document.querySelectorAll("a").forEach(preventDefaultClick);
+
+  document.querySelectorAll(".dropdown-btn").forEach(toggleSitemapHeader);
 };
+
+const toggleSitemapHeader = (el) =>
+  el.addEventListener("click", (e) => {
+    const next = e.target.nextElementSibling;
+    next.classList.toggle("hidden");
+  });
+
+const preventDefaultClick = (el) =>
+  el.addEventListener("click", (e) => {
+    e.preventDefault();
+  });
 
 window.addEventListener("DOMContentLoaded", start);
