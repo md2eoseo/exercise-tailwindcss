008da466ec3e11770a96831519db84eb59b93f9c
md2eoseo <md2eoseo@gmail.com>
2020-10-16 13:19:54 +0900

    bag view (added curtain)

--- public/script.js
+++ public/script.js
@@ -2,28 +2,33 @@
 
 const start = () => {
   console.log("Hello Tailwindcss!");
-  document.querySelectorAll("a").forEach(preventDefaultClick);
-  document.querySelectorAll(".dropdown-btn").forEach(toggleSitemapHeader);
-  document.querySelectorAll(".bag-view-btn").forEach(toggleBagView);
+  document.querySelectorAll("a").forEach((el) =>
+    el.addEventListener("click", (e) => {
+      e.preventDefault();
+    })
+  );
+  document
+    .querySelectorAll(".dropdown-btn")
+    .forEach((el) => el.addEventListener("click", toggleSitemapHeader));
+  document
+    .querySelectorAll(".bag-view-btn")
+    .forEach((el) => el.addEventListener("click", toggleBagView));
+  document
+    .querySelector(".bag-view-curtain")
+    .addEventListener("click", toggleBagView);
 };
 
-const toggleBagView = (el) =>
-  el.addEventListener("click", (e) => {
-    const bagView = document.querySelector(".bag-view");
-    bagView.classList.toggle("hidden");
-  });
+const toggleBagView = () => {
+  const bagView = document.querySelector(".bag-view");
+  bagView.classList.toggle("hidden");
+};
 
-const toggleSitemapHeader = (el) =>
-  el.addEventListener("click", (e) => {
-    const next = e.target.nextElementSibling;
-    next.classList.toggle("hidden");
-    next.classList.toggle("dropdown-menu-animation");
-    el.classList.toggle("dropdown-btn-animation");
-  });
-
-const preventDefaultClick = (el) =>
-  el.addEventListener("click", (e) => {
-    e.preventDefault();
-  });
+const toggleSitemapHeader = (e) => {
+  const el = e.target;
+  const next = e.target.nextElementSibling;
+  next.classList.toggle("hidden");
+  next.classList.toggle("dropdown-menu-animation");
+  el.classList.toggle("dropdown-btn-animation");
+};
 
 window.addEventListener("DOMContentLoaded", start);
